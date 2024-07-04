1e397812543aced12d9a1d587a5090eb3dee8df2
rlAlan <momoriallan@gmail.com>
2024-07-04 18:34:37 +0100

    setup conan and install packages

---
+++ include/ParticleSim.h
@@ -0,0 +1,8 @@
+#ifndef PARTICLE_SIM_H
+#define PARTICLE_SIM_H
+
+class ParticleSim{
+
+};
+
+#endif // PARTICLE_SIM_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <fmt/core.h>
+#include <GLFW/glfw3.h>
 
+/*
+ * add opengl
+ * Make a window that works
+ * add eigen
+ * */
 
 int main(){
   fmt::print("Hey there");
