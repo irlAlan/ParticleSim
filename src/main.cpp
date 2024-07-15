96002afea6738e3ce2cece5ef16bdff41066d0a6
irlAlan <momoriallan@gmail.com>
2024-07-15 22:45:36 +0100

    decided against doing both projects at once, I will be entierly focused on getting this working alone

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,6 @@
-#include <VEngine/VoxelEngine.h>
 #include <fmt/core.h>
 
 int main() {
-  VEngine::Window win{"hey there", {450, 340}};
-
-  while(win.windowIsOpen()){
-    win.displayWindow();
-  }
-
-
+  fmt::print("is working");
   return 0;
 }
